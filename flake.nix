e75c239d29569e4988e3e413db474c9339f46413
mimifuwacc <mail@mimifuwa.cc>
2026-07-21 00:38:42 +0900

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "aarch64-darwin";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        pkgs.bun
+      ];
+    };
+  };
+}
